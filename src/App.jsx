c17e5339b4a8911f71ef3f3391eaca9233cba7e9
Sam Hadfield <sthtech@protonmail.com>
2026-02-17 16:42:28 -0600

    feat: route components, add nav bar in App.jsx
    Route components through main.jsx, add <nav> in App.jsx to replace
    old NavBar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
-import NavBar from "./Components/NavBar/NavBar.jsx";
+import { Link } from "react-router";
+import { Outlet } from "react-router";
 
 function App() {
   return (
-    <header>
-      <NavBar />
-    </header>
+    <>
+      <header>
+        <h1>Sam's Dev Store</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="shop">Shop</Link>
+          <Link to="cart">Cart</Link>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App.jsx";
+import Home from "./Components/Home/Home.jsx";
+import Shop from "./Components/Shop/Shop.jsx";
+import Cart from "./Components/Cart/Cart.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "shop", element: <Shop /> },
+      { path: "cart", element: <Cart /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
