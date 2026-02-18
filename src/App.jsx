75155560992e1a86e8330dfcc084638d11810ec2
Sam Hadfield <sthtech@protonmail.com>
2026-02-18 08:59:08 -0600

    feat: add classes to links and adjust link color

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import { Outlet } from "react-router";
 
 function App() {
   return (
-    <>
+    <body>
       <header>
         <h1>Sam's Dev Store</h1>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="shop">Shop</Link>
-          <Link to="cart">Cart</Link>
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link to="shop" className="link">
+            Shop
+          </Link>
+          <Link to="cart" className="link">
+            Cart
+          </Link>
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
-    </>
+    </body>
   );
 }
 
