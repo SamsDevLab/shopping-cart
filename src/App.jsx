9c265f54804918c97be3b3363eb39409cf158c34
Sam Hadfield <sthtech@protonmail.com>
2026-02-18 09:56:08 -0600

    feat: add footer with wireframe styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <body>
       <header>
-        <h1>Sam's Dev Store</h1>
+        <h1>Sam's Great Outdoors</h1>
         <nav>
           <Link to="/" className="link">
             Home
@@ -22,6 +22,35 @@ function App() {
       <main>
         <Outlet />
       </main>
+      <footer>
+        <div className="title-and-mission">
+          <h2>Sam's Great Outdoors</h2>
+          <p>It ain't gonna make you proficient - but it will help.</p>
+        </div>
+        <div className="all-links">
+          <h2>Links</h2>
+          <Link to="/" className="link">
+            Home
+          </Link>
+          <Link to="shop" className="link">
+            Shop
+          </Link>
+          <Link to="cart" className="link">
+            Cart
+          </Link>
+        </div>
+        <div className="contact-info">
+          <h2>Contact Us</h2>
+          <div className="email">
+            <img src="" alt="" />
+            <h3>Email: justgonnasendit@fakecompany.com</h3>
+          </div>
+          <div className="phone">
+            <img src="" alt="" />
+            <h3>Phone: 555.555.5555</h3>
+          </div>
+        </div>
+      </footer>
     </body>
   );
 }
