ec68df487538c3abfad88b320b53e461de558a04
Sam Hadfield <sthtech@protonmail.com>
2026-03-02 11:54:44 -0600

    feat: add input blur event for number persistence
    Add input blur event/tests so that number sets input value when blur event
    occurs.

--- src/Components/Shop/ProductCard/ProductCard.jsx
+++ src/Components/Shop/ProductCard/ProductCard.jsx
@@ -8,13 +8,28 @@ const ProductCard = () => {
     if (!(inputValue <= 1)) setInputValue(inputValue - 1);
   };
 
-  const handleInputChange = (event) => {
-    if (event.target.value >= 1 && event.target.value <= 99)
-      setInputValue(event.target.value);
+  const handleIncrementChange = () => {
+    if (inputValue >= 1 && inputValue < 99) setInputValue(inputValue + 1);
   };
 
-  const handleIncrementChange = () => {
-    if (inputValue >= 0) setInputValue(inputValue + 1);
+  /* 
+  Edge cases to address: 
+  • Can add (multiple) 0s -before- the number
+  • Can add a decimal after the first number
+  • May be able to handle this with a blur event or adding to the cart - or both
+  • Separate edge case: when you backspace in the input it will result in 0 rather than a blank space
+  */
+
+  const handleInputChange = (event) => {
+    const newNum = Number(event.target.value);
+
+    if (newNum >= 0 && newNum <= 99) setInputValue(newNum);
+  };
+
+  const handleBlurEvent = (event) => {
+    const newNumber = Number(event.target.value);
+
+    setInputValue(newNumber);
   };
 
   return (
@@ -24,13 +39,17 @@ const ProductCard = () => {
         <label className={styles.inputLabel} htmlFor="quantity">
           Quantity
         </label>
-        <button onClick={handleDecrementChange}>–</button>
+        <button onClick={handleDecrementChange}>-</button>
         <input
           type="number"
           id="quantity"
+          maxLength="2"
+          step="1"
+          pattern="[1-9][0-9]"
           value={inputValue}
-          onChange={(event) => handleInputChange(event)}
           className={styles.quantityInput}
+          onChange={(event) => handleInputChange(event)}
+          onBlur={(event) => handleBlurEvent(event)}
         />
         <button onClick={handleIncrementChange}>+</button>
       </div>
--- src/Components/Shop/ProductCard/ProductCard.test.jsx
+++ src/Components/Shop/ProductCard/ProductCard.test.jsx
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import ProductCard from "./ProductCard";
@@ -8,15 +8,16 @@ describe("ProductCard", () => {
     render(<ProductCard />);
     const user = userEvent.setup();
 
-    const button = screen.getByRole("button", { name: "–" });
+    const button = screen.getByRole("button", { name: "-" });
     const input = screen.getByRole("spinbutton", { name: "Quantity" });
+
     await user.type(input, "2");
 
-    const inputValue = Number(input.value);
+    const originalInputValue = Number(input.value);
 
     await user.click(button);
 
-    expect(Number(input.value)).toBeLessThan(inputValue);
+    expect(Number(input.value)).toBeLessThan(originalInputValue);
   });
 
   it("should increase the input value", async () => {
@@ -41,5 +42,22 @@ describe("ProductCard", () => {
     await user.type(input, "2");
 
     expect(input).toHaveValue(Number("12"));
+  });
+
+  // Start here Monday - figure out how to test for the blur event and parse the numbers
+  it("should leave the typed input number after blur event", async () => {
+    render(<ProductCard />);
+    const user = userEvent.setup();
+
+    const input = screen.getByRole("spinbutton", { name: "Quantity" });
+
+    await user.clear(input);
+
+    await user.type(input, "23");
+
+    await user.tab();
+
+    expect(input).not.toHaveFocus();
+    expect(input).toHaveValue(Number("23"));
   });
 });
