b436fb7b3e4aa62dc013f8695e53fce475695a1a
Sam Hadfield <sthtech@protonmail.com>
2026-03-04 08:18:40 -0600

    feat: add testing file for App component

---
+++ src/App.test.jsx
@@ -0,0 +1,8 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+// Start here tomorrow morning - you're throwing an error indicating that your <Link>
+// tag is being rendered outside of either a <BrowserRouter> or a <MemoryRouter>
+// Plus, not sure if the below mock is even set up right.
+// Tackle this first thing in the morning with fresh eyes
