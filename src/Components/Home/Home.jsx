be0a238fec35348f9a90cbca66c6f77987352d30
Sam Hadfield <sthtech@protonmail.com>
2026-03-06 09:29:16 -0600

    feat: filter highest-rated products
    Filter out only the highest-rated products from the productList to display
    on Home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,33 @@
 import styles from "./Home.module.css";
+import ProductCard from "../ProductCard/ProductCard";
+import { useOutletContext } from "react-router";
 
 const Home = () => {
+  const [productList, setProductList] = useOutletContext();
+  const favoriteItems = productList.filter((product) => {
+    if (product.rating.rate >= 4.5) {
+      return product;
+    }
+  });
+
   return (
-    // Start here tomorrow (02/18 - Figure out a layout plan for component and decide what
-    // you want to include
     <>
       <h2 className={styles.homeHeader}>Greetings from Sam's Shop</h2>
-      <p>Welcome to the shop-a-thon!</p>
+      <p>Top Rated Items</p>
+      {favoriteItems.map((item) => {
+        return (
+          <ProductCard
+            data-testid="product-card"
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+            category={item.category}
+            image={item.image}
+            rating={item.rating}
+          />
+        );
+      })}
     </>
   );
 };
