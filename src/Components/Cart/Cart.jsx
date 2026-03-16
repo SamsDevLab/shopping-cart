bd1b48e99ece997064c61223d971e4f40f521aa7
Sam Hadfield <sthtech@protonmail.com>
2026-03-16 08:58:11 -0500

    refactor: correct sales tax, convert nums to strs
    Correct sales tax value as it was previously including subtotal and
    shipping.
    
    Ensure that all pricing is converted a string with two decimal places
    before being placed in elements

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -12,17 +12,48 @@ const Cart = () => {
     }
   });
 
+  const subtotal = cartProducts
+    .map((product) => {
+      return product.price * product.quantity;
+    })
+    .reduce((acc, current) => {
+      const newTotal = acc + current;
+
+      return newTotal;
+    }, 0);
+
+  const getSalesTax = () => {
+    const taxRate = 0.06;
+
+    let totalWithTax = subtotal * taxRate;
+
+    return Math.round(totalWithTax * 100) / 100;
+  };
+
+  const getShipping = () => {
+    if (cartProducts.length > 0) {
+      return 10;
+    } else return 0;
+  };
+
+  const shipping = getShipping();
+  const salesTax = getSalesTax();
+  const total = subtotal + shipping + salesTax;
+
   return (
     <>
       <h2>Shopping Cart</h2>
       <section className={styles.checkoutContainer}>
         {cartProducts.length > 0 ? (
-          <section className={styles.productSection}>
+          <section
+            className={styles.productSection}
+            data-testid="cart-container"
+          >
             {cartProducts.map((product) => {
               return (
                 <CheckoutCard
                   className={styles.checkoutCard}
-                  keys={product.id}
+                  key={product.id}
                   props={product}
                 />
               );
@@ -31,11 +62,14 @@ const Cart = () => {
         ) : (
           <h2>No items added to your basket</h2>
         )}
-        <section
-          className={styles.summaryContainer}
-          data-testid="cart-container"
-        >
-          <OrderSummary className={styles.orderSummary} />
+        <section className={styles.summaryContainer}>
+          <OrderSummary
+            className={styles.orderSummary}
+            subtotal={subtotal}
+            shipping={shipping}
+            tax={salesTax}
+            total={total}
+          />
         </section>
       </section>
     </>
--- src/Components/OrderSummary/OrderSummary.jsx
+++ src/Components/OrderSummary/OrderSummary.jsx
@@ -1,24 +1,29 @@
 import styles from "./OrderSummary.module.css";
 
 const OrderSummary = ({ subtotal, shipping, tax, total }) => {
+  const subtotalStr = subtotal.toFixed(2);
+  const shippingStr = shipping.toFixed(2);
+  const taxStr = tax.toFixed(2);
+  const totalStr = total.toFixed(2);
+
   return (
     <section className={styles.orderSummary} data-testid="order-summary">
       <h2>Order Summary</h2>
       <div className={styles.summaryContainer}>
         <h3>Subtotal</h3>
-        <p data-testid="subtotal">${subtotal}</p>
+        <p data-testid="subtotal">${subtotalStr}</p>
       </div>
       <div className={styles.summaryContainer}>
         <h3>Shipping</h3>
-        <p data-testid="shipping">${shipping}</p>
+        <p data-testid="shipping">${shippingStr}</p>
       </div>
       <div className={styles.summaryContainer}>
         <h3>Sales Tax </h3>
-        <p data-testid="tax">${tax}</p>
+        <p data-testid="tax">${taxStr}</p>
       </div>
       <div className={styles.summaryContainer}>
         <h3>Total</h3>
-        <p data-testid="total">${total}</p>
+        <p data-testid="total">${totalStr}</p>
       </div>
     </section>
   );
